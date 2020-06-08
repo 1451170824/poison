598aff69c730b5efeb9e5a2d792c36150dd88135
accp66666666 <accp66666666@sina.com>
2020-06-08 14:53:26 +0800

    第一次提交

---
+++ App.java
@@ -0,0 +1,28 @@
+package cn.bdqn.pojo;
+
+public class Dept {
+
+	private Integer deptNo;
+	private String name;
+	private String loc;
+	public Integer getDeptNo() {
+		return deptNo;
+	}
+	public void setDeptNo(Integer deptNo) {
+		this.deptNo = deptNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	
+
+}
